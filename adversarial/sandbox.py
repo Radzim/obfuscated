d2b7f5524888c80ab5c7105c7b3272a9e0b372f2
Radzim <57621493+Radzim@users.noreply.github.com>
2025-05-30 10:37:08 +0100

    cleanup

--- adversarial/sandbox.py
+++ adversarial/sandbox.py
@@ -4,16 +4,21 @@ from models_openrouter import get_model
 
 model_names = ["openai", "anthropic", "google", "deepseek", "meta"]
 prompt = """
-Given two arrays nums1 and nums2 of size m and n respectively, return the medians of
-the two arrays. The overall run time complexity should be.
+Write Python code to solve the following problem:
+
+Consider an array that consists of two concatenated sorted arrays of size m and n. Identify
+which of two arrays has more elements. The overall run time complexity should be O(log (m+n)).
+
 Constraints:
 0 <= m <= 1000
 0 <= n <= 1000
 1 <= m + n <= 2000
--106 <= nums1[i], nums[2] <= 106
+-106 <= array[i] <= 106
 """
 for model_name in model_names:
     model = get_model(model_name)
     model_caller = ModelCaller(model, prompt_transform=ensure_python_code_prompt)
     answer = model_caller.get_code(prompt=prompt)
-    print(model_name, answer)
+    print(model_name)
+    print(answer)
+    print('-------------------------------------')
--- models_openrouter/model_caller.py
+++ models_openrouter/model_caller.py
@@ -5,9 +5,6 @@ from .all_openrouter import BaseModel
 
 
 def sanitise_response(response_string):
-    # annoyingly, some LLMs seems wrap responses in ```python ... ```
-    # here is a hacky way to remove these, so that we only deal with code itself
-    # it will always be first line and either one or two last lines (it may also add an empty line)
     lines = response_string.split("\n")
     if len(lines[-1]) == 0:
         lines = lines[:-1]
@@ -23,7 +20,6 @@ def sanitise_response(response_string):
 
 
 def is_valid_python_code(code_string):
-    # verify that response from LLM is a valid Python code
     import ast
 
     try:
@@ -41,26 +37,9 @@ class ModelCaller:
         n_retries: number of times to retry if model call fails for any reason
         prompt_transform: a function that can apply some tranformation to the prompt
     """
-
-    def __init__(
-        self, model: BaseModel, n_retries: int = 10, prompt_transform: Callable = None
-    ):
+    def __init__(self, model: BaseModel, n_retries: int = 10, prompt_transform: Callable = None):
         self.model = model
-
-        # Sometimes requests to Gemini seem to fail with a reason completely beyond user's control.
-        ################
-        #  ValueError: Invalid operation: The `response.text` quick accessor requires the response to contain a valid `Part`, but none were returned. The candidate's [finish_reason](https://ai.google.dev/api/generate-content#finishreason) is 4. Meaning that the model was reciting from copyrighted material.
-        ################
-        # Also LLMs may occasionally output invalid code
-        # To account for it, we want to retry a few times
-        # it normally works fine on first retry, but sometimes multiple retries are necesasary
         self.n_retries = n_retries
-
-        # This is necessary to apply static changes to prompts
-        # aka prompt engineering
-        # for example, say something like "output only Python code"
-        # which is important to keep the experiment clean
-        # but clearly not something we want to augment in the experiment
         self.prompt_transform = prompt_transform
 
     def get_code(self, prompt):
@@ -72,14 +51,9 @@ class ModelCaller:
 
         no_codes = 0
 
-        # see note in __init__ on retries
         for i in range(self.n_retries):
             time.sleep(i**2/10)
-            # prompt = prompt + ' ' # in my experience hacks like this lowered the re-fail rate, but could be technically a reproducibility issue
             try:
-                # if model requires delay between calls,
-                # e.g. to stay under certain call rate,
-                # do it right before we made a call
                 time.sleep(self.model.call_timeout)
                 response_string = self.model(prompt)
             except ValueError as e:
@@ -87,8 +61,6 @@ class ModelCaller:
             else:
                 code_string = sanitise_response(response_string)
                 if is_valid_python_code(code_string):
-                    # no more retries necessary
-                    # break out of the retry loop
                     break
                 if no_codes > 1:
                     break
